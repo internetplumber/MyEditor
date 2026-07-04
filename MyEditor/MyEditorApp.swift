c9690eceb7d80f57e2101b41e595f6283bd9389f
Rob Evans <rhe@internetplumber.org.uk>
2026-07-04 21:49:42 +0100

    Initial Commit

---
+++ MyEditor/MyEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEditorApp.swift
+//  MyEditor
+//
+//  Created by Rob Evans on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
